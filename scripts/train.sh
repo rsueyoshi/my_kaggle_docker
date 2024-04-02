800a27c56036dc7fbc143ab6ce4ebf3fb70297c9
Ubuntu <ubuntu@ip-172-31-77-132.ec2.internal>
2024-04-02 00:38:39 +0000

    微修正

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-python train_4folds_freeze.py --config yaml/deberta3large_0966_bf16_4folds1_freeze.yaml > /kaggle/output/deberta3large_fold1_freeze6/log.txt
-python train_4folds_freeze.py --config yaml/deberta3large_0966_bf16_4folds2_freeze.yaml > /kaggle/output/deberta3large_fold2_freeze6/log.txt
+python train_alldata.py --config yaml/deberta3large_0966_bf16_all_freeze.yaml > /kaggle/output/deberta3large_freeze6/log.txt
+kaggle datasets create -p /kaggle/output/deberta3large_freeze6 --dir-mode zip
 aws ec2 stop-instances --instance-ids i-0449cb0b94ddaa813	
