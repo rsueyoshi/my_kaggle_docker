51cf29ca75594439b0e00378f707baffc27a04cd
sue1242 <sueyoshi124@gmail.com>
2024-04-03 23:22:26 +0900

    AWPをオリジナルの2回backward版に変更

--- scripts/utils/trainer_utils.py
+++ scripts/utils/trainer_utils.py
@@ -9,60 +9,61 @@ if is_apex_available():
     from apex import amp
 
 class AWP:
-    def __init__(self, model, optimizer, adv_param='weight',
-                 adv_lr=0.001, adv_eps=0.001):
+    def __init__(
+        self,
+        model,
+        optimizer,
+        compute_loss_fn,
+        adv_param="weight",
+        adv_lr=1,
+        adv_eps=0.2,
+    ):
         self.model = model
         self.optimizer = optimizer
+        self.compute_loss_fn = compute_loss_fn
         self.adv_param = adv_param
         self.adv_lr = adv_lr
         self.adv_eps = adv_eps
         self.backup = {}
+        self.backup_eps = {}
 
-    def perturb(self):
-        """
-        Perturb model parameters for AWP gradient
-        Call before loss and loss.backward()
-        """
-        self._save()  # save model parameters
-        self._attack_step()  # perturb weights
+    def __call__(self, inputs):
+        self._save()
+        self._attack_step() 
+        adv_loss = self.compute_loss_fn(self.model, inputs)   
+        adv_loss.backward()  
+        self._restore()
 
     def _attack_step(self):
         e = 1e-6
         for name, param in self.model.named_parameters():
             if param.requires_grad and param.grad is not None and self.adv_param in name:
-                grad = self.optimizer.state[param]['exp_avg']
-                norm_grad = torch.norm(grad)
-                norm_data = torch.norm(param.detach())
-
-                if norm_grad != 0 and not torch.isnan(norm_grad):
-                    # Set lower and upper limit in change
-                    limit_eps = self.adv_eps * param.detach().abs()
-                    param_min = param.data - limit_eps
-                    param_max = param.data + limit_eps
-
-                    # Perturb along gradient
-                    # w += (adv_lr * |w| / |grad|) * grad
-                    param.data.add_(grad, alpha=(self.adv_lr * (norm_data + e) / (norm_grad + e)))
-
-                    # Apply the limit to the change
-                    param.data.clamp_(param_min, param_max)
+                norm1 = torch.norm(param.grad)
+                norm2 = torch.norm(param.data.detach())
+                if norm1 != 0 and not torch.isnan(norm1):
+                    r_at = self.adv_lr * param.grad / (norm1 + e) * (norm2 + e)
+                    param.data.add_(r_at)
+                    param.data = torch.min(
+                        torch.max(param.data, self.backup_eps[name][0]), self.backup_eps[name][1]
+                    )
 
     def _save(self):
         for name, param in self.model.named_parameters():
             if param.requires_grad and param.grad is not None and self.adv_param in name:
                 if name not in self.backup:
-                    self.backup[name] = param.clone().detach()
-                else:
-                    self.backup[name].copy_(param.data)
+                    self.backup[name] = param.data.clone()
+                    grad_eps = self.adv_eps * param.abs().detach()
+                    self.backup_eps[name] = (
+                        self.backup[name] - grad_eps,
+                        self.backup[name] + grad_eps,
+                    )
 
-    def restore(self):
-        """
-        Restore model parameter to correct position; AWP do not perturbe weights, it perturb gradients
-        Call after loss.backward(), before optimizer.step()
-        """
+    def _restore(self,):
         for name, param in self.model.named_parameters():
             if name in self.backup:
-                param.data.copy_(self.backup[name])
+                param.data = self.backup[name]
+        self.backup = {}
+        self.backup_eps = {}
 
 
 class Trainer_Awp(Trainer):
@@ -104,7 +105,14 @@ class Trainer_Awp(Trainer):
 
     def create_optimizer_and_scheduler(self, num_training_steps: int):
         super().create_optimizer_and_scheduler(num_training_steps)
-        self.awp = AWP(self.model, self.optimizer, self.adv_param, self.adv_lr, self.adv_eps)
+        self.awp = AWP(
+            self.model, 
+            self.optimizer, 
+            self.compute_loss, 
+            self.adv_param, 
+            self.adv_lr, 
+            self.adv_eps
+        )
 
     def training_step(self, model, inputs):
         """
@@ -112,10 +120,6 @@ class Trainer_Awp(Trainer):
         """
         model.train()
         inputs = self._prepare_inputs(inputs)
-
-        # AWP実行
-        if self.state.epoch >= self.awp_start:
-            self.awp.perturb()
 
         if is_sagemaker_mp_enabled():
             from transformers.trainer_pt_utils import smp_forward_backward
@@ -133,5 +137,9 @@ class Trainer_Awp(Trainer):
                 scaled_loss.backward()
         else:
             self.accelerator.backward(loss)
+
+        # AWP実行
+        if self.state.epoch >= self.awp_start:
+            self.awp(inputs)
 
         return loss.detach() / self.args.gradient_accumulation_steps
