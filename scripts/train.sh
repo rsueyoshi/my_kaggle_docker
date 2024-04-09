d1022a641422c94c86909839c651911b865d4c60
Ubuntu <ubuntu@ip-172-31-77-132.ec2.internal>
2024-04-09 00:31:54 +0000

    微調整

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
-python /kaggle/scripts/train_4folds_freeze.py --config /kaggle/scripts/yaml/EX007.yaml > /kaggle/output/EX007/log.txt
-kaggle datasets create -p /kaggle/output/EX007 --dir-mode zip > /kaggle/output/EX007/log2.txt
+python train_alldata.py --config /kaggle/scripts/yaml/EX008.yaml > /kaggle/output/EX008/log.txt
+python train_alldata.py --config /kaggle/scripts/yaml/EX009.yaml > /kaggle/output/EX009/log.txt
+kaggle datasets create -p /kaggle/output/EX008 --dir-mode zip > /kaggle/output/EX008/log2.txt
+kaggle datasets create -p /kaggle/output/EX009 --dir-mode zip > /kaggle/output/EX009/log2.txt
 aws ec2 stop-instances --instance-ids i-0449cb0b94ddaa813	
