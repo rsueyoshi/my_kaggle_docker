c3304e16625c6123d44ac39b1a9e87c3a6f19376
Ubuntu <ubuntu@ip-172-31-77-132.ec2.internal>
2024-04-09 00:47:02 +0000

    微調整

--- scripts/kaggle.sh
+++ scripts/kaggle.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-# kaggle datasets create -p /home/ubuntu/my_kaggle_docker/output/EX006 --dir-mode zip > /home/ubuntu/my_kaggle_docker/output/EX006/log2.txt
-kaggle datasets create -p /home/ubuntu/my_kaggle_docker/output/EX007 --dir-mode zip > /home/ubuntu/my_kaggle_docker/output/EX007/log2.txt
-aws ec2 stop-instances --instance-ids i-0449cb0b94ddaa813	
+kaggle datasets create -p /kaggle/output/EX006 --dir-mode zip > /kaggle/output/EX006/log2.txt
+kaggle datasets create -p /kaggle/output/EX007 --dir-mode zip > /kaggle/output/EX007/log2.txt
+# aws ec2 stop-instances --instance-ids i-0449cb0b94ddaa813
