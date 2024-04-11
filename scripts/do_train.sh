b8d8bb481991c1547dd9c1570839b61e1ad87898
Ubuntu <ubuntu@ip-172-31-77-132.ec2.internal>
2024-04-11 14:41:51 +0000

    tmp

--- scripts/do_train.sh
+++ scripts/do_train.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # python train_alldata.py --config /kaggle/scripts/yaml/EX013.yaml > /kaggle/output/EX013/log.txt
-python train_4folds_freeze.py --config /kaggle/scripts/yaml/EX012.yaml > /kaggle/output/EX012/log.txt
+# python train_4folds_freeze.py --config /kaggle/scripts/yaml/EX012.yaml > /kaggle/output/EX012/log.txt
 # python train_4folds_freeze.py --config /kaggle/scripts/yaml/EX011.yaml > /kaggle/output/EX011/log.txt
 # python train_alldata.py --config /kaggle/scripts/yaml/EX010.yaml > /kaggle/output/EX010/log.txt
 kaggle datasets create -p /kaggle/output/EX012 --dir-mode zip > /kaggle/output/EX012/log2.txt
