03ae189a43c7849d3526db254fee8f24eda23e71
sue1242 <sueyoshi124@gmail.com>
2024-04-14 22:37:47 +0900

    AWP修正

--- scripts/do_train.sh
+++ scripts/do_train.sh
@@ -1,11 +1,9 @@
 #/bin/bash
 
-python train_4folds_freeze.py --config /kaggle/scripts/yaml/EX016.yaml > /kaggle/output/EX016/log.txt
-python train_4folds_freeze.py --config /kaggle/scripts/yaml/EX017.yaml > /kaggle/output/EX017/log.txt
-python train_alldata.py --config /kaggle/scripts/yaml/EX021.yaml > /kaggle/output/EX021/log.txt
-python train_alldata.py --config /kaggle/scripts/yaml/EX022.yaml > /kaggle/output/EX022/log.txt
-kaggle datasets create -p /kaggle/output/EX016 --dir-mode zip > /kaggle/output/EX016/log2.txt
-kaggle datasets create -p /kaggle/output/EX017 --dir-mode zip > /kaggle/output/EX017/log2.txt
-kaggle datasets create -p /kaggle/output/EX021 --dir-mode zip > /kaggle/output/EX021/log2.txt
-kaggle datasets create -p /kaggle/output/EX022 --dir-mode zip > /kaggle/output/EX022/log2.txt
+python train_4folds_freeze_AWP2 --config /kaggle/scripts/yaml/EX023.yaml > /kaggle/output/EX023/log.txt
+python train_4folds_freeze_AWP2 --config /kaggle/scripts/yaml/EX024.yaml > /kaggle/output/EX024/log.txt
+python train_4folds_freeze_AWP2 --config /kaggle/scripts/yaml/EX025.yaml > /kaggle/output/EX025/log.txt
+kaggle datasets create -p /kaggle/output/EX023 --dir-mode zip > /kaggle/output/EX023/log2.txt
+kaggle datasets create -p /kaggle/output/EX024 --dir-mode zip > /kaggle/output/EX024/log2.txt
+kaggle datasets create -p /kaggle/output/EX025 --dir-mode zip > /kaggle/output/EX025/log2.txt
 aws ec2 stop-instances --instance-ids i-0449cb0b94ddaa813
