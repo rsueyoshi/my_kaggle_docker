6d1eade7ae2a4720df98b46f3e334fd85539aa41
Ubuntu <ubuntu@ip-172-31-77-132.ec2.internal>
2024-04-14 06:46:58 +0000

    微修正

--- scripts/utils/collator.py
+++ scripts/utils/collator.py
@@ -9,7 +9,8 @@ class Collator:
         output = dict()
         output["input_ids"] = [sample["input_ids"] for sample in batch]
         output["attention_mask"] = [sample["attention_mask"] for sample in batch]
-        output["token_type_ids"] = [sample["token_type_ids"] for sample in batch]
+        if "token_type_ids" in batch[0]:
+            output["token_type_ids"] = [sample["token_type_ids"] for sample in batch]
         output["labels"] = [sample["labels"] for sample in batch]
 
         # calculate max token length of this batch
@@ -19,18 +20,21 @@ class Collator:
         if self.tokenizer.padding_side == "right":
             output["input_ids"] = [s + (batch_max - len(s)) * [self.tokenizer.pad_token_id] for s in output["input_ids"]]
             output["attention_mask"] = [s + (batch_max - len(s)) * [0] for s in output["attention_mask"]]
-            output["token_type_ids"] = [s + (batch_max - len(s)) * [0] for s in output["token_type_ids"]]
+            if "token_type_ids" in batch[0]:
+                output["token_type_ids"] = [s + (batch_max - len(s)) * [0] for s in output["token_type_ids"]]
             output["labels"] = [s + (batch_max - len(s)) * [-100] for s in output["labels"]]
         else:
             output["input_ids"] = [(batch_max - len(s)) * [self.tokenizer.pad_token_id] + s for s in output["input_ids"]]
             output["attention_mask"] = [(batch_max - len(s)) * [0] + s for s in output["attention_mask"]]
-            output["token_type_ids"] = [(batch_max - len(s)) * [0] + s for s in output["token_type_ids"]]
+            if "token_type_ids" in batch[0]:
+                output["token_type_ids"] = [(batch_max - len(s)) * [0] + s for s in output["token_type_ids"]]
             output["labels"] = [(batch_max - len(s)) * [-100] + s for s in output["labels"]]
 
         # convert to tensors
         output["input_ids"] = torch.tensor(output["input_ids"], dtype=torch.long)
         output["attention_mask"] = torch.tensor(output["attention_mask"], dtype=torch.long)
-        output["token_type_ids"] = torch.tensor(output["token_type_ids"], dtype=torch.long)
+        if "token_type_ids" in batch[0]:
+            output["token_type_ids"] = torch.tensor(output["token_type_ids"], dtype=torch.long)
         output["labels"] = torch.tensor(output["labels"], dtype=torch.long)
 
         return output
