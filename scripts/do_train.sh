7c2e4c40b4debee8514e06f0a8d1c9d1670a8e01
Ubuntu <ubuntu@ip-172-31-77-132.ec2.internal>
2024-04-18 13:58:49 +0000

    tmp

--- scripts/do_train.sh
+++ scripts/do_train.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 
-python train_4folds_freeze_AWP2.py --config /kaggle/scripts/yaml/EX032.yaml > /kaggle/output/EX032/log.txt
 python train_4folds_freeze_AWP2.py --config /kaggle/scripts/yaml/EX033.yaml > /kaggle/output/EX033/log.txt
 python train_4folds_freeze_AWP2.py --config /kaggle/scripts/yaml/EX034.yaml > /kaggle/output/EX034/log.txt
 python train_4folds_freeze_AWP2.py --config /kaggle/scripts/yaml/EX031.yaml > /kaggle/output/EX031/log.txt
